7ca81d9acfbf1e002643b8b3e638962dcad10e37
roy-cruz <roy.cruz@upr.edu>
2024-09-01 22:05:46 +0200

    trigger rate now obtained w/  DIALS OMS proxy; auto dials setup func

---
+++ src/dqmexplore/__init__.py
@@ -0,0 +1,6 @@
+import dqmexplore.dataproc
+import dqmexplore.exploreutils
+import dqmexplore.omsutils
+import dqmexplore.plotting
+import dqmexplore.statplotting
+import dqmexplore.trends
--- src/dqmexplore/exploreutils.py
+++ src/dqmexplore/exploreutils.py
@@ -1,4 +1,8 @@
-def check_empty_lss(me_dict, thrshld=10):
+from dqmexplore.dataproc import generate_me_dict
+import pandas as pd
+
+def check_empty_lss(me_df, thrshld=0):
+    me_dict = generate_me_dict(me_df)
     empty_me_dict = {}
     for me in list(me_dict.keys()):
         empty_me_dict[me] = {}
@@ -6,4 +10,4 @@ def check_empty_lss(me_dict, thrshld=10):
         for i, entries in enumerate(me_dict[me]["entries"]):
             if entries <= thrshld:
                 empty_me_dict[me]["empty_lss"].append(i + 1)
-    return empty_me_dict
+    return pd.DataFrame(empty_me_dict).T
