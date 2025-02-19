a4b28ee22e04750c48388759e315268c70ca495b
roy <roy.cruz@upr.edu>
2025-02-19 07:41:47 -0600

    Added CH, RR json scripts

---
+++ scripts/getCHjson.py
@@ -0,0 +1,19 @@
+from dqmexplore.utils.datautils import loadFromWeb
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="A script to get CertHelper data downloaded into a JSON file"
+)
+parser.add_argument(
+    "--fname",
+    type=str,
+    default="./jsons/ch_refruns.json",
+    help="Name and path of output file.",
+)
+args = parser.parse_args()
+
+print("Downloading CH run data and storing it in ch_refruns.json")
+
+url = "https://certhelper.web.cern.ch/certify/allRunsRefRuns/"
+
+loadFromWeb(url, args.fname)
